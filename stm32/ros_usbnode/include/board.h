fbaad943a35f87aad237256df99d4a3629ab18ca
Jeremy Salwen <jeremysalwen@gmail.com>
2025-07-20 02:03:45 -0400

    Increase max charge amperage to 1.5

--- stm32/ros_usbnode/include/board.h
+++ stm32/ros_usbnode/include/board.h
@@ -95,10 +95,10 @@ extern "C"
 
 //#define I_DONT_NEED_MY_FINGERS              1      // disables EmergencyController() (no wheel lift, or tilt sensing and stopping the blade anymore)
 
-/// nominal max charge current is 1.0 Amp
-#define MAX_CHARGE_CURRENT 1.0f
-/// Max voltage allowed
-#define MAX_CHARGE_VOLTAGE 29.0f
+/// nominal max charge current is 1.5 Amp
+#define MAX_CHARGE_CURRENT 1.5f
+/// Max voltage allowed 29.4
+#define MAX_CHARGE_VOLTAGE 29.4f
 /// Voltage threshold for CC to CV transition
 #define LIMIT_VOLTAGE_150MA 28.0f
 /// Default max battery voltage allowed
